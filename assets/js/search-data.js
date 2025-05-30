f76269f1b2f2a727f6e2efdc05799a43977195b7
NerdyVisky <vishvesh106@gmail.com>
2025-05-30 07:04:03 +0000

    Deploying to gh-pages from @ NerdyVisky/nerdyvisky.github.io@f66af809cec6fed362893008adf365229715a86c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,37 +42,7 @@ ninja.data = [{
           section: "News",},{id: "news-won-1st-runners-up-award-at-the-annual-threesis-academic-challenge-in-nyu-gsas-and-a-cash-prize-of-1300-know-more-about-the-nyu-gsas-threesis-academic-challenge",
           title: 'Won 1st Runners Up award at the annual Threesis Academic Challenge in NYU...',
           description: "",
-          section: "News",},{id: "projects-do-retrieval-heads-speak-the-same-language",
-          title: 'Do retrieval heads speak the same language?',
-          description: "This project analyzes retrieval heads in multilingual LLMs using Needle-in-a-Haystack tasks across English, German, and Chinese. We find that strong retrieval heads are largely language-agnostic and critical for performance. Masking them leads to significant accuracy drops, offering insights for optimizing KV caching and multilingual model efficiency.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-attention-aware-dpo-for-reducing-hallucinations-in-multi-image-qa",
-          title: 'Attention-Aware DPO for Reducing Hallucinations in Multi-Image QA',
-          description: "We introduce an attention-aware, multi-image augmented preference alignment method that improves accuracy by 8.5%, and further enhance inference-time alignment through adaptive attention scaling, yielding a 10% performance gain over the base model.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-clinicalml-a-medically-interpretable-ml-pipeline-for-clinical-outcome-prediction-using-mimic-iii-discharge-summaries",
-          title: 'ClinicalML - A medically interpretable ML pipeline for clinical outcome prediction using MIMIC-III...',
-          description: "ClinicalML is an interpretable machine learning pipeline for predicting ICU patient outcomes—mortality and length of stay—using admission-time clinical notes. It extracts disease and drug entities, reduces dimensionality with BioClinicalBERT embeddings, and applies traditional ML models. ClinicalML performs comparably to BERT-based models while offering greater transparency and clinical trust.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
+          section: "News",},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
@@ -82,6 +52,26 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
+            },},{id: "projects-clinicalml-a-medically-interpretable-ml-pipeline-for-clinical-outcome-prediction-using-mimic-iii-discharge-summaries",
+          title: 'ClinicalML - A medically interpretable ML pipeline for clinical outcome prediction using MIMIC-III...',
+          description: "ClinicalML is an interpretable machine learning pipeline for predicting ICU patient outcomes—mortality and length of stay—using admission-time clinical notes. It extracts disease and drug entities, reduces dimensionality with BioClinicalBERT embeddings, and applies traditional ML models. ClinicalML performs comparably to BERT-based models while offering greater transparency and clinical trust.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/clinicalML/";
+            },},{id: "projects-towards-synthetic-data-augmentation-for-lecture-slide-understanding",
+          title: 'Towards synthetic data augmentation for Lecture Slide Understanding',
+          description: "SynSlideGen is a synthetic data generation pipeline that creates realistic, annotated lecture slides through an LLM-powered generation pipeline. Designed to support tasks like slide element detection and retrieval, it leverages structured text to generate diverse layouts and semantic content. SynSlideGen addresses the scarcity of annotated educational data through scalable automation.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/synslidegen/";
+            },},{id: "projects-do-retrieval-heads-speak-the-same-language",
+          title: 'Do retrieval heads speak the same language?',
+          description: "This project analyzes retrieval heads in multilingual LLMs using Needle-in-a-Haystack tasks across English, German, and Chinese. We find that strong retrieval heads are largely language-agnostic and critical for performance. Masking them leads to significant accuracy drops, offering insights for optimizing KV caching and multilingual model efficiency.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Multilingual_Retrieval_Heads/";
+            },},{id: "projects-attention-aware-dpo-for-reducing-hallucinations-in-multi-image-qa",
+          title: 'Attention-Aware DPO for Reducing Hallucinations in Multi-Image QA',
+          description: "We introduce an attention-aware, multi-image augmented preference alignment method that improves accuracy by 8.5%, and further enhance inference-time alignment through adaptive attention scaling, yielding a 10% performance gain over the base model.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/AttnDPO/";
             },},{
         id: 'social-email',
         title: 'email',
